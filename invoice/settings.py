09aa348396463e0a6635b7dad1e4d8f34129a06f
FRANCIS <francisganyo64@gmail.com>
2025-06-25 17:31:03 +0000

    updates

--- invoice/settings.py
+++ invoice/settings.py
@@ -84,8 +84,12 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'invoicedb',
+        'USER': 'invoiceuser',
+        'PASSWORD': 'invoice',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
