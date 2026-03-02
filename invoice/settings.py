271154317910f96eb8dca3e10fa0f9989773e8ae
Odhisika <francisganyo64@gmail.com>
2026-03-02 16:57:11 +0000

    updates

--- invoice/settings.py
+++ invoice/settings.py
@@ -82,23 +82,23 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'invoicedb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'osikani',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'invoicedb',
+        'USER': 'postgres',
+        'PASSWORD': 'osikani',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
-}   
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }   
 
 
 
