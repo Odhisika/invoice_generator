71da33ef341b6180905bec10bf3a994cf0480716
Odhisika <francisganyo64@gmail.com>
2026-03-03 12:35:30 +0000

    updates

--- invoice/settings.py
+++ invoice/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'home',
+    'django.contrib.humanize',
+    'home',
     'accounts'
-    
 ]
 
 MIDDLEWARE = [
